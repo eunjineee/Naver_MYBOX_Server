43ce2bf5d10782ce39c4e95ce30a8809ff9d4994
eunjineee <yej6642@naver.com>
2023-08-14 10:38:10 +0900

    Feat : 추가 user entity

---
+++ mybox/src/main/java/back/server/mybox/user/User.java
@@ -0,0 +1,21 @@
+package back.server.mybox.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId; //pk
+    @Column(length = 32, nullable = false)
+    private String username; //아이디
+    private String password;
+    private String nickname;
+
+}
--- mybox/src/main/java/back/server/mybox/user/user.java
+++
@@ -1,4 +0,0 @@
-package back.server.mybox.user;
-
-public class user {
-}
